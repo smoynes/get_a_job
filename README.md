252b0c50a2bb1d46c1bb249ce8510953601ec3b9
Scott Moynes <scott.moynes@gmail.com>
2013-10-18 15:09:42 -0400

    Add basic project structure.

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 Get A Job
 =========
 
-An Python implementation of Steve Klabnik's "Get A Job" service from
-_Designing Hypermedia APIs_ by Steve Klabnik.
+An Python implementation of "Get A Job" service from _Designing
+Hypermedia APIs_ by Steve Klabnik.
+
+Installation
+------------
+
+From source:
+
+    $ python setup.py install
+
+Development
+-----------
+
+    $ virtualenv env
+    $ source env/bin/activate
+    $ pip install -r requirements.txt
+    $ python setup.py develop
+
+Running tests:
+
+    $ python setup.py test
---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+requirements = [
+    'Flask==0.10.1',
+    'Flask-RESTful==0.2.5',
+]
+
+test_requirements = [
+    'nose==1.3.0',
+]
+
+setup(
+    name='get_a_job',
+    version='0.0.1',
+    description='Hypermedia Job Service',
+    long_description=readme,
+    author='Scott Moynes',
+    author_email='scott.moynes@gmail.com',
+    url='https://github.com/smoynes/get_a_job',
+    license=license,
+    packages=find_packages(exclude=['tests']),
+    test_suite='nose.collector',
+    install_requires=requirements,
+    setup_requires=test_requirements,
+)
