a89e7f9f625427d558300eb5e5cbc2881cdcc207
Scott Moynes <scott.moynes@gmail.com>
2013-10-24 02:10:09 +0000

    Add optional configuration customization of app.

--- get_a_job/__init__.py
+++ get_a_job/__init__.py
@@ -4,9 +4,10 @@ from flask.ext.restful import Api
 from .models import db
 from .api import configure_api
 
-def create_app(object_name):
+def create_app(object_name, **kwargs):
     app = Flask(object_name)
     app.config.from_object(object_name)
+    app.config.update(kwargs)
 
     db.init_app(app)
     configure_api(app)
